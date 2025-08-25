a6f2a17cae995769b3133ebfc6784c29e13ea7c5
coder-gf <kritichandel2001@gmail.com>
2025-08-25 07:37:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ const roadMusic = document.getElementById('road-music');
 const mallMusic = document.getElementById('mall-music');
 const roomMusic = document.getElementById('room-music');
 
+function stopAllMusic() {
+    startMusic.pause();
+    epilogueMusic.pause();
+    dateMusic.pause();
+    candyMusic.pause();
+    libraryMusic.pause();
+    roadMusic.pause();
+    mallMusic.pause();
+    roomMusic.pause();
+}
+
 /* Best effort: try to autoplay; if the browser blocks it, begin on first user gesture */
 function tryPlay(audio){
   if (!audio) return;
